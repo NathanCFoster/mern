859c8bce1b101ade46a54717634f23220188f731
NathanCFoster <nathancfoster.03@gmail.com>
2021-10-28 11:02:45 -0600

    firstJSX

--- react/reactApp/reactapp/src/App.js
+++ react/reactApp/reactapp/src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+      <p className="display-2">Hello Dojo!</p>
+      <p className="display-3">Things I need to do:</p>
+      <ul className="d-flex flex-wrap flex-column align-content-start">
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
     </div>
   );
 }
