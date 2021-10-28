e56b3e1f63a92ccb8147407b8cb2da65eb2dd705
NathanCFoster <nathancfoster.03@gmail.com>
2021-10-28 12:19:00 -0600

    3rd times the charm

--- react/reactApp/propit/src/components/PersonCard.jsx
+++ react/reactApp/propit/src/components/PersonCard.jsx
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
 
-class PersonCard extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            age: this.props.person.age
-        };
-    }
-    addAge = () => {
-        var newAge = parseInt(this.state.age);
-        this.setState({ age: newAge + 1});
-    }
-    render(){
-        return(
-            <div className="container">
-                <p className="display-3">{this.props.person.firstName}, {this.props.person.lastName}</p>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {this.props.person.hairColor}</p>
-                <button onClick={ this.addAge } className="btn-outline-dark btn">It's your birthday</button>
-            </div>
-        );
-    }
+const PersonCard = props => {
+    // constructor = props => {
+    //     // super(props);
+    //     this.state = {
+    //         age: this.props.person.age
+    //     };
+    // }
+    // this.addAge = () => {
+    //     var newAge = parseInt(this.state.age);
+    //     this.setState({ age: newAge + 1});
+    // }
+    return(
+        <div className="container">
+            <p className="display-3">{props.person.firstName}, {props.person.lastName}</p>
+            <p>Age: {props.person.age}</p>
+            <p>Hair Color: {props.person.hairColor}</p>
+            {/* <button onClick={ this.addAge } className="btn-outline-dark btn">It's your birthday</button> */}
+        </div>
+    );
 }
 
 export default PersonCard;
