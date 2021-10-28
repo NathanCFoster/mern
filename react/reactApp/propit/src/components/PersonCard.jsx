afd6cbed9798e0e978e846fb548160837c2285db
NathanCFoster <nathancfoster.03@gmail.com>
2021-10-28 11:56:06 -0600

    putit

--- react/reactApp/propit/src/components/PersonCard.jsx
+++ react/reactApp/propit/src/components/PersonCard.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.person.age
+        };
+    }
+    addAge = () => {
+        var newAge = parseInt(this.state.age);
+        this.setState({ age: newAge + 1});
+    }
     render(){
         return(
             <div className="container">
                 <p className="display-3">{this.props.person.firstName}, {this.props.person.lastName}</p>
-                <p>Age: {this.props.person.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.person.hairColor}</p>
+                <button onClick={ this.addAge } className="btn-outline-dark btn">It's your birthday</button>
             </div>
         );
     }
