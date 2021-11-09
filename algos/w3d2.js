876f3f0552ee5abf2ef620fc513e51a24b86ba5a
NathanCFoster <nathancfoster.03@gmail.com>
2021-11-09 11:12:10 -0700

    captions

--- algos/w3d2.js
+++ algos/w3d2.js
@@ -31,12 +31,17 @@ const students = [{
 ];
 
 function findByIdAndUpdate(id, updatedVals, collection) {
-    const obj = collection.find(e => e.id == id);
+    //iterates through the entire collection and finds the first iteration where
+    //id == the provieded param id
+    const obj = collection.find(e => e.id == id);   
+    //converts the updated vals object into an array so that we can iterate through it easier
     const entries = Object.entries(updatedVals);
-    console.log(entries)
+    //updates the object inside the collection
     obj[entries[0][0]] = entries[0][1];
+    //returns the object
     return obj;
 }
+
 
 console.log(findByIdAndUpdate(3, { redBeltStatus: true }, students))
 
